186d107f3b764c14313409ced2d0c532ca955436
Simon Feltman <sfeltman@src.gnome.org>
2013-10-11 17:39:31 -0700

    cache refactoring: Move PyGIHashCache and related marshaling into new file
    Massive re-org of hash table arg cache and its marshaling by moving all
    related code fragments into an isolated file where most functions are made
    static. pygi-arg-hashtable.h exposes a single function:
    pygi_arg_hash_table_new_from_info. This is all the caching system needs to
    produce the proper bits for handling hash table marshaling.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=709700

---
+++ gi/pygi-arg-ghashtable.h
@@ -0,0 +1,37 @@
+/* -*- Mode: C; c-basic-offset: 4 -*-
+ * vim: tabstop=4 shiftwidth=4 expandtab
+ *
+ * Copyright (C) 2013 Simon Feltman <sfeltman@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ */
+
+#ifndef __PYGI_ARG_HASHTABLE_H__
+#define __PYGI_ARG_HASHTABLE_H__
+
+#include <girepository.h>
+#include "pygi-cache.h"
+
+G_BEGIN_DECLS
+
+PyGIArgCache *pygi_arg_hash_table_new_from_info (GITypeInfo    *type_info,
+                                                 GIArgInfo     *arg_info,   /* may be null */
+                                                 GITransfer     transfer,
+                                                 PyGIDirection  direction);
+
+G_END_DECLS
+
+#endif /*__PYGI_ARG_HASHTABLE_H__*/
